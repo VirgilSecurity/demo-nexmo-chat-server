f7c526e00e404d913c32e97082bbb0270a35f7c3
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-08-23 14:44:39 +0300

    remove virgil auth config from setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -71,56 +71,25 @@ inquirer.prompt([
         validate: input => HEX_REGEXP.test(input) || 'Invalid Nexmo API Secret. Must be a string in HEX'
     }
 ]).then(answers => {
-    Object.assign(answers, {
-        virgilAuth: generateVirgilAuthKeyPair()
-    });
     const parsedNexmoConfig = parseNexmoAppConfig(
         fs.readFileSync(answers.nexmo.appConfigPath, { encoding: 'utf8' })
     );
     Object.assign(answers.nexmo, parsedNexmoConfig);
     
-    const configPaths = {
-        auth: path.resolve(process.cwd(), '.env.auth'),
-        web: path.resolve(process.cwd(), '.env.web'),
-        test: path.resolve(process.cwd(), '.env.test')
-    };
-
+    const configPath = path.resolve(process.cwd(), '.env');
     const relativeToCwd = absolute => path.relative(process.cwd(), absolute);
 
-    return Promise.all([
-        writeFileAsync(
-            configPaths.auth,
-            generateVirgilAuthConfig(answers)
-        ),
-        writeFileAsync(
-            configPaths.web,
-            generateWebConfig(answers)
-        ),
-        writeFileAsync(
-            configPaths.test,
-            generateTestConfig(answers)
-        )
-    ]).then(() => {
+    return writeFileAsync(configPath, generateConfig(answers)).then(() => {
         console.log(chalk`
         {bold The server is set up}
-        Virgil Auth config written to {cyan ${relativeToCwd(configPaths.auth)}}
-        Main Web app config written to {cyan ${relativeToCwd(configPaths.web)}}
-        Test config written to {cyan ${relativeToCwd(configPaths.test)}}
+        Config written to {cyan ${relativeToCwd(configPath)}}
 
-        Run {bold docker-compose up} to start the server
+        Run {bold npm start} to start the server
         `);
     });
 }).catch(error => {
     console.log(chalk.red('Unexpected error occurred'), error);
 });
-
-function generateVirgilAuthKeyPair () {
-    const authKeyPair = virgilCrypto.generateKeys();
-    return {
-        privateKey: virgilCrypto.exportPrivateKey(authKeyPair.privateKey).toString('base64'),
-        publicKey: virgilCrypto.exportPublicKey(authKeyPair.publicKey).toString('base64')
-    };
-}
 
 function parseNexmoAppConfig (src) {
     return src.split('\n').reduce((result, line) => {
@@ -142,38 +111,15 @@ function parseNexmoAppConfig (src) {
     }, {});
 }
 
-function generateVirgilAuthConfig (rootConfig) {
-    return Object.entries({
-        'APP_ID': rootConfig.virgil.appId,
-        'API_KEY_ID': rootConfig.virgil.apiKeyId,
-        'API_KEY': rootConfig.virgil.apiKey,
-        'KEY': rootConfig.virgilAuth.privateKey,
-        'DB': 'mongodb://mongo:27017/virgil-auth'
-    })
-    .map(entry => entry.join('='))
-    .join('\n');
-}
-
-function generateWebConfig (rootConfig) {
+function generateConfig (rootConfig) {
     return Object.entries({
         'VIRGIL_APP_ID': rootConfig.virgil.appId,
         'VIRGIL_API_KEY_ID': rootConfig.virgil.apiKeyId,
         'VIRGIL_API_KEY': rootConfig.virgil.apiKey,
-        'VIRGIL_AUTH_URL': 'http://auth:8080',
-        'VIRGIL_AUTH_PUBLIC_KEY': rootConfig.virgilAuth.publicKey,
         'NEXMO_API_KEY': rootConfig.nexmo.apiKey,
         'NEXMO_API_SECRET': rootConfig.nexmo.apiSecret,
         'NEXMO_APP_ID': rootConfig.nexmo.app_id,
         'NEXMO_APP_PRIVATE_KEY': Buffer.from(rootConfig.nexmo.private_key, 'utf8').toString('base64')
-    })
-    .map(entry => entry.join('='))
-    .join('\n');
-}
-
-function generateTestConfig (rootConfig) {
-    return Object.entries({
-        'VIRGIL_AUTH_URL': 'http://localhost:8080',
-        'VIRGIL_AUTH_PUBLIC_KEY': rootConfig.virgilAuth.publicKey
     })
     .map(entry => entry.join('='))
     .join('\n');
